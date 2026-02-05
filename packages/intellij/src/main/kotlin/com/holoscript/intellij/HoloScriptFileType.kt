690ba9b425dc7789bbaf5059767d7cd4780eaffa
brianonbased-dev <brianonbased@gmail.com>
2026-02-05 13:47:49 -0700

    feat: implement 5 remaining TODOs
    - IntelliJ: Add SVG icons and load with IconLoader (holoscript, object, trait, function, property)
    - OpenXRCompiler: Add Vulkan VkInstance/VkPhysicalDevice/VkDevice bindings
    - OpenXRCompiler: Add OpenGL ES EGLDisplay/EGLContext/EGLConfig bindings
    - WASMCompiler: Implement decodePayload for memory payload decoding (null, number, boolean, string, JSON, Float32Array)
    - SemanticValidator: Add deep method signature validation (param count, types, return type)
    - HoloCompositionParser: Add parsePrimitiveObject for primitive#id and primitive #id { } syntax
    
    All 1876 core tests passing

--- packages/intellij/src/main/kotlin/com/holoscript/intellij/HoloScriptFileType.kt
+++ packages/intellij/src/main/kotlin/com/holoscript/intellij/HoloScriptFileType.kt
@@ -21,17 +21,18 @@ class HoloScriptFileType private constructor() : LanguageFileType(HoloScriptLang
     
     override fun getDefaultExtension(): String = "hsplus"
     
-    override fun getIcon(): Icon? = HoloScriptIcons.FILE
+    override fun getIcon(): Icon = HoloScriptIcons.FILE
 }
 
 /**
  * Icons for HoloScript
  */
 object HoloScriptIcons {
-    // TODO: Load actual icon resources
-    val FILE: Icon? = null
-    val OBJECT: Icon? = null
-    val TRAIT: Icon? = null
-    val FUNCTION: Icon? = null
-    val PROPERTY: Icon? = null
+    private fun loadIcon(path: String): Icon = com.intellij.openapi.util.IconLoader.getIcon(path, HoloScriptIcons::class.java)
+    
+    val FILE: Icon = loadIcon("/icons/holoscript.svg")
+    val OBJECT: Icon = loadIcon("/icons/object.svg")
+    val TRAIT: Icon = loadIcon("/icons/trait.svg")
+    val FUNCTION: Icon = loadIcon("/icons/function.svg")
+    val PROPERTY: Icon = loadIcon("/icons/property.svg")
 }
